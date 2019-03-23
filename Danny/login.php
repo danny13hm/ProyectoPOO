9e946462b99e6da81bd96b7e392d249dac8f7459
danny13hm <danny13hm@hotmail.com>
2019-03-22 19:42:44 -0600

    Nuevo Login

---
+++ Danny/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>LinkedIn</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/login.css">
+    
+</head>
+<body>
+    <header id="barra" style="height:8px">
+
+    </header>
+
+
+    <main class="centrado">        
+
+        <div id="logo2">
+        
+            <h6 style="font-size:20px">Linked</h6>
+            <img id ="img-login" src="img/favicon.ico">
+        </div>
+
+        <div id="div-texto" class="centrado">
+        <!-- para el texto de bienvenida-->
+        <h1>¡Te damos la bienvenida de nuevo!</h1>
+        <p>¿Te vas a perder tu siguiente gran oportunidad? Inicia sesión para estar al día de tu entorno profesional.</p>
+        </div>
+
+        <div>
+            <form action="">
+
+            </form>
+        </div>       
+    </main>
+
+
+    <footer>
+
+    </footer>
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/login.js"></script>
+</body>
+</html>
